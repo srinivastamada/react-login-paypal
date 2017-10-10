aea5804bd2485d12d61ba6cfc9b7cb6fabe016cb
Srinivas Tamada <srinivas@9lessons.info>
2017-10-10 15:37:31 -0400

    first

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="row " id="Body">
+        <div className="medium-12 columns">
+          <h2 id="welcomeText">Facebook Login with React</h2>
+
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import './Welcome.css';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="row " id="Body">
+        <div className="medium-12 columns">
+          <h2 id="welcomeText">Facebook Login with React</h2>
+
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
